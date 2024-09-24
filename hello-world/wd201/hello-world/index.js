45e93fa35cc0d6a180a5883c3a1aaf423e31caf9
Sriram <sriramknr63@gmail.com>
2024-09-24 21:07:52 +0530

    Your commit message

--- hello-world/wd201/hello-world/index.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-fs.writeFile(
-  "sample.txt",
-  "Hello World. Welcome to Node.js File System module.",
-  (err) => {
-    if (err) throw err;
-    console.log("File created!");
-  } 
-);
